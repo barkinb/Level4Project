6906876be0225732f138cda41cdcc714ac37ef66
Barkın Bryce <2452842b@student.gla.ac.uk>
2024-01-22 19:13:01 +0000

    implemented a method to sort the points in tangents

--- src/app/bezierCurve.py
+++ src/app/bezierCurve.py
@@ -1,6 +1,6 @@
 from tkinter import Canvas
 from math import sqrt
-from maths_functions import ni,basis
+from maths_functions import *
 ##DISCLAIMER = a portion of code in this file originates from https://github.com/otto-kokstein/bezierve-v2
 NUMBER_OF_DETAIL = 10000
 DEFAULT_CURVE_WIDTH = 3
@@ -19,6 +19,7 @@ class BezierCurve:
         self.curve = None
 
     def draw(self,canvas: Canvas) -> None:
+        self.sort_points()
         if self.curve is not None:
             self.canvas.delete(self.curve)
         curve_points = []
@@ -32,11 +33,19 @@ class BezierCurve:
             *curve_points, width=self.curve_width, fill=self.curve_colour
 
         )
+    def sort_points(self):
 
+        initial_point = self.points[0]
+
+        # calculate all angles and sort
+        sorted_points = sorted(self.points, key=lambda p: calculate_angle(p, initial_point))
+
+        unique_points = [sorted_points[0]] + [p for i, p in enumerate(sorted_points[1:]) if p != sorted_points[i]]
+
+        self.points = unique_points
     def calculate_curve_point(self, t: float) :
         point_x = 0.0
         point_y = 0.0
-
 
         if len(self.points) == 2:
             point_x = (1 - t) * self.points[0][0] + t * self.points[
