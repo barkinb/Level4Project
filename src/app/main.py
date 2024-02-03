e250510ba140ec3217bad81ae846e33960fff9d4
Barkın Bryce <2452842b@student.gla.ac.uk>
2024-02-03 17:02:28 +0000

    moving an axis point will no longer bezier curve and the axis points are stored correctly

--- src/app/main.py
+++ src/app/main.py
@@ -5,9 +5,10 @@ from PIL import Image, ImageTk
 from Axis import BezierCurve
 from DistributionParser import parse_distribution
 
+
 class NomogramApp:
     def __init__(self, root):
-        self.current_control_point_id = None
+        self.current_point_id = None
         self.start_y = None
         self.start_x = None
         self.button_status = tk.NORMAL
@@ -60,12 +61,11 @@ class NomogramApp:
             self.distribution_entry.pack(side=tk.LEFT, padx=2, pady=2, fill=tk.X)
             self.save_distribution_button = tk.Button(toolbar_frame, text="Save Distribution",
                                                       command=self.save_distribution)
-            
+
             self.save_distribution_button.pack(side=tk.LEFT, padx=2, pady=2, fill=tk.X)
             toolbar_frame.pack(side=tk.TOP, fill=tk.X)
         except Exception as e:
             print("Error loading icon images:", e)
-
 
     def select_image_file(self):
         file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png *.jpg *.jpeg")])
@@ -113,32 +113,36 @@ class NomogramApp:
             self.display_bezier_coordinates()
 
             point_size = 5
-            control_point_id = f"{axis_id}_{len(self.control_points[axis_id]) - 1}"
+            control_point_id = f"control{axis_id}_{len(self.control_points[axis_id]) - 1}"
 
             self.canvas.create_oval(
                 x - point_size, y - point_size, x + point_size, y + point_size,
-                fill="red", outline="black", tags=(axis_id, control_point_id, "control_point")
+                fill="red", outline="black", tags=(control_point_id, "control_point")
             )
 
             # Bind events only for the newly created control point
-            self.canvas.tag_bind(control_point_id, "<Button-1>",
-                                 lambda event, axis=axis_id, id=control_point_id: self.start_drag(event, axis, id))
-            self.canvas.tag_bind(control_point_id, "<B1-Motion>",
-                                 lambda event, axis=axis_id, id=control_point_id: self.drag(event, axis, id))
-            self.canvas.tag_bind(control_point_id, "<ButtonRelease-1>",
-                                 lambda event, axis=axis_id, id=control_point_id: self.stop_drag(event, axis, id))
+            self.move_point( control_point_id, axis_id)
+
         if axis_id in self.curve_objects:  # if there is already a bezier curve
             self.draw_bezier(axis_id)
         self.canvas.unbind("<Button-1>")
 
-    def start_drag(self, event, axis_id, control_point_id):
+    def move_point(self, point_id, axis_id):
+        self.canvas.tag_bind(point_id, "<Button-1>",
+                             lambda event, axis=axis_id, id=point_id: self.start_drag(event, axis))
+        self.canvas.tag_bind(point_id, "<B1-Motion>",
+                             lambda event, axis=axis_id, id=point_id: self.drag(event, axis, id))
+        self.canvas.tag_bind(point_id, "<ButtonRelease-1>",
+                             lambda event, axis=axis_id, id=point_id: self.stop_drag(axis, id))
+
+    def start_drag(self, event, point_id):
         # Record the starting position of the control point
         ## adapted from https://stackoverflow.com/questions/29789554/tkinter-draw-rectangle-using-a-mouse
         self.start_x = self.canvas.canvasx(event.x)
         self.start_y = self.canvas.canvasy(event.y)
-        self.current_control_point_id = control_point_id
+        self.current_point_id = point_id
 
-    def drag(self, event, axis_id, control_point_id):
+    def drag(self, event, axis_id, point_id):
         # Move the control point based on the mouse movement
         cur_x = self.canvas.canvasx(event.x)
         cur_y = self.canvas.canvasy(event.y)
@@ -146,25 +150,32 @@ class NomogramApp:
         delta_y = cur_y - self.start_y
 
         # Move the control point and update the starting position
-        self.canvas.move(control_point_id, delta_x, delta_y)
+        self.canvas.move(point_id, delta_x, delta_y)
         self.start_x = cur_x
         self.start_y = cur_y
-
+        point_index = int(point_id.split('_')[-1])
         # Update the control point's position in the data structure
-        control_point_index = int(control_point_id.split('_')[-1])
-        self.control_points[axis_id][control_point_index] = (cur_x, cur_y)
+        if "control" in point_id:
+            # Update the control point's position in the data structure
 
-        # Update the Bézier curve at the end
-        self.update_bezier(axis_id)
+            self.control_points[axis_id][point_index] = (cur_x, cur_y)
+            self.update_bezier(axis_id)
 
-    def stop_drag(self, event, axis_id, control_point_id):
+        elif "axis" in point_id:
+            self.axis_points[axis_id][point_index] = (cur_x, cur_y, self.axis_points[axis_id][point_index][-1])
+
+    def stop_drag(self, axis_id, point_id):
         # Update the control point's position in the data structure
-        x, y = self.canvas.coords(control_point_id)[0], self.canvas.coords(control_point_id)[1]
+        x, y = self.canvas.coords(point_id)[0], self.canvas.coords(point_id)[1]
 
-        control_point_index = int(control_point_id.split('_')[-1])
-        self.control_points[axis_id][control_point_index] = (x, y)
+        point_index = int(point_id.split('_')[-1])
 
-        self.display_bezier_coordinates()
+        if "control" in point_id:
+            self.control_points[axis_id][point_index] = (x, y)
+            self.display_bezier_coordinates()
+        elif "axis" in point_id:
+            self.axis_points[axis_id][point_index] = (x, y, self.axis_points[axis_id][point_index][-1])
+            self.display_axis_coordinates()
 
     def display_bezier_coordinates(self):
         # prints the control points to console
@@ -182,14 +193,14 @@ class NomogramApp:
             coordinates_str = ""
             for axis_id, coords in self.axis_points.items():
                 coordinates_str += f"{axis_id}:\n"
-                coordinates_str += '\n'.join([f"  ({x}, {y}, {point_value})" for x, y,point_value in coords])
+                coordinates_str += '\n'.join([f"  ({x}, {y}, {point_value})" for x, y, point_value in coords])
                 coordinates_str += "\n\n"
             print(coordinates_str)
 
     def draw_bezier(self, bezier_axis_id=None):
         if bezier_axis_id is None:
             bezier_axis_id = simpledialog.askstring("Enter Axis ID", "Enter the name of the axis you wish to draw:")
-            while bezier_axis_id not in self.control_points:
+            if bezier_axis_id not in self.control_points:
                 bezier_axis_id = simpledialog.askstring("Axis Not Found",
                                                         "Enter the name of the axis you wish to draw:")
 
@@ -224,19 +235,21 @@ class NomogramApp:
             if point_value is not None:
                 if axis_id not in self.control_points.keys():
                     messagebox.showwarning("Curve Not Found", f"Bezier curve for axis '{axis_id}' does not exist.")
-
-                if axis_id not in self.axis_points.keys():
+                    pass
+                elif axis_id not in self.axis_points.keys():
                     self.axis_points[axis_id] = []
                 self.axis_points[axis_id].append((x, y, point_value))
 
                 self.display_axis_coordinates()
-                point_size = 5
-                axis_point_id = f"{axis_id}_{len(self.axis_points[axis_id]) - 1}"
+                point_size = 2.5
+                axis_point_id = f"axis{axis_id}_{len(self.axis_points[axis_id]) - 1}"
 
                 self.canvas.create_oval(
                     x - point_size, y - point_size, x + point_size, y + point_size,
                     fill="red", outline="black", tags=(axis_point_id, "axis_point")
                 )
+                self.move_point( axis_point_id, axis_id)
+            self.canvas.unbind("<Button-1>")
 
     def save_distribution(self):
         axis_id = simpledialog.askstring("Enter Axis ID", "Enter the identifier for the axis:")
@@ -246,7 +259,6 @@ class NomogramApp:
             print(self.distributions[axis_id])
             messagebox.showinfo("Distribution Saved",
                                 f"Statistical distribution '{distribution}' saved for axis '{axis_id}'.")
-
 
     def load_project(self):
         pass
