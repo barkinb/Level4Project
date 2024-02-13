3b1c062bce66a5b5a6223cbee260a59cc467ef52
Barkın Bryce <2452842b@student.gla.ac.uk>
2024-02-13 22:20:45 +0000

    added more statistical distributions

--- src/app/DistributionParser.py
+++ src/app/DistributionParser.py
@@ -1,7 +1,6 @@
 import scipy.stats as stats
 import re
 
-
 def parse_distribution(distribution_str):
     try:
         match = re.match(r'(\w+)\((.*?)\)', distribution_str)
@@ -10,18 +9,40 @@ def parse_distribution(distribution_str):
             params = [float(param) for param in match.group(2).split(',')]
 
             if dist_type == 'Normal':
-                return stats.norm(*params)
+                distribution = stats.norm(*params)
             elif dist_type == 'Uniform':
-                return stats.uniform(*params)
+                distribution = stats.uniform(*params)
             elif dist_type == 'Poisson':
-                return stats.poisson(*params)
+                distribution = stats.poisson(*params)
             elif dist_type == 't':
-                return stats.t(*params)
-
+                distribution = stats.t(*params)
+            elif dist_type == 'Exponential':
+                distribution = stats.expon(*params)
+            elif dist_type == 'Gamma':
+                distribution = stats.gamma(*params)
+            elif dist_type == 'Beta':
+                distribution = stats.beta(*params)
+            elif dist_type == 'Binomial':
+                distribution = stats.binom(*params)
+            elif dist_type == 'Geometric':
+                distribution = stats.geom(*params)
+            elif dist_type == 'Hypergeometric':
+                distribution = stats.hypergeom(*params)
+            elif dist_type == 'Logistic':
+                distribution = stats.logistic(*params)
+            elif dist_type == 'Lognormal':
+                distribution = stats.lognorm(*params)
+            elif dist_type == 'NegativeBinomial':
+                distribution = stats.nbinom(*params)
+            elif dist_type == 'UniformDiscrete':
+                distribution = stats.randint(*params)
+            elif dist_type == 'Wald':
+                distribution = stats.wald(*params)
+            elif dist_type == 'Weibull':
+                distribution = stats.weibull_min(*params)
             else:
                 raise ValueError('Distribution could not be parsed correctly')
-            
+            print(distribution)
+            return distribution
     except Exception as e:
         print(f"Error parsing distribution: {e}")
-
-
